41df25db55a373f21ca9da624cd7e4a148c495b1
williamedge <williamedge88@gmail.com>
2023-09-20 14:49:24 +0800

    new version

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ wutils.
 A Python package for doing things I do too often to re-type. Pronounced "woo-tills".
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.2"
 __author__ = 'William Edge'
 __credits__ = 'This ones all me'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='d2spike',
-    version='0.0.1',    
+    version='0.0.2',    
     description='A Python package for de-spiking velocity data.',
     url='https://github.com/williamedge/d2spike',
     author='William Edge',
